86cbbc3205f11f8dfa137c36d8a1a68067b112d4
Tania Masset <taniamasset@gmail.com>
2022-08-04 18:38:11 -0300

    Tarea Unidad 4: Integración de Node.js con Base de datos (parte 2)

---
+++ Mi proyecto -Tierras-/backend/models/usuariosModel.js
@@ -0,0 +1,15 @@
+var pool = require('./bd'); //llamando datos bd
+var md5 = require('md5');
+
+async function getUserByUsernameAndPassword(user, password){
+    try{
+        var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query, [user, md5(password)]);
+        console.log(query);
+        return rows [0];
+    }catch (error){
+        console.log(error);
+    }
+}
+
+module.exports = {getUserByUsernameAndPassword}
---
+++ Mi proyecto -Tierras-/backend/routes/admin/novedades.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require ('./../../models/usuariosModel');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/novedades',{
+    layout: 'admin/layout', 
+    persona: req.session.nombre});
+    
+});
+module.exports = router;
